33c8bf3ed549f2104b322e09e54bc86ae5ea7205
David Ladaev <174save@gmail.com>
2024-03-27 17:08:58 +0200

    Added About page and made changes in App.js

--- magazine/src/App.js
+++ magazine/src/App.js
@@ -1,13 +1,17 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import Products from './pages/Products';
+import About from './pages/About';
 
 export default function App(){
     return(
         <>
-        <Route path="/">
-            <Products />
-        </Route>
+        <Router>
+            <Routes>
+                <Route exact path="/" element={<Products/>} /> 
+                <Route path="/about" element={<About />}/> 
+            </Routes>
+        </Router>
         </>
     )
 }
---
+++ magazine/src/pages/About.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Menu from "../components/topMenu";
+
+export default function About(){
+    return(
+        <>
+        <Menu />
+        </>
+    )
+}
