f44d361805a3bb5440721207b6ddfda31fa93565
David Ladaev <174save@gmail.com>
2024-03-31 10:57:54 +0300

    change the input's of form

--- magazine/src/components/form.js
+++ magazine/src/components/form.js
@@ -1,19 +1,27 @@
 import React from "react";
-import { useForm, SubmitHandler } from "react-hook-form"; //for comfortable work with form
+import { useForm } from "react-hook-form"; //for comfortable work with form
 import './form.css'
 
 export default function Form(){
-   
+   const {
+    register, 
+    formState:{
+        errors,
+    },
+    handleSubmit,
+   } = useForm();
+
+   const onSubmit = (data) => console.log(data)
 
     return(
         <div className="formBlock">
-            <form className="form">
-                <input className="item" name="name" type="text" placeholder="First name..."></input>
-                <input className="item" name="surname" type="text" placeholder="Last name..."></input>
-                <input className="item" name="phone" type="tel" placeholder="Phone number..."></input>
-                <input className="item" name="email" type="email" placeholder="Email..." pattern=".+@example\.com"></input>
-                <input className="item" name="address" type="text" placeholder="Address..." pattern="Example, example, 6"></input>
-                <input className="item" name="postcode" type="text" placeholder="Post-code..."></input>
+            <form onSubmit={handleSubmit(onSubmit)} className="form">
+                <input {...register('name', {required: true})} className="item" type="text" placeholder="First name..."></input>
+                <input {...register('surname', {required: true})} className="item"  type="text" placeholder="Last name..."></input>
+                <input {...register('phone', {required: true, minLength:{value: 10}, maxLength:{value: 13}})} className="item" type="tel" placeholder="Phone number..."></input>
+                <input {...register('email', {required: true, pattern:/^[\w\.-]+@[a-zA-Z\d\.-]+\.[a-zA-Z]{2,}$/})} className="item" type="email" placeholder="Email..."></input>
+                <input {...register('address', {required: true})} className="item" type="text" placeholder="Address..."></input>
+                <input {...register('postcode', {required: true, minLength:{value: 6}, maxLength:{value: 6}})} className="item"  type="text" placeholder="Post-code..."></input>
                 <button className="button" type="submit">Send</button>
             </form>
         </div>
