850998619b08053b45a3ba1940e62317463dab22
David Ladaev <174save@gmail.com>
2024-04-04 09:25:32 +0300

    animation and fetch card

--- magazine/src/components/poductCard.js
+++ magazine/src/components/poductCard.js
@@ -1,9 +1,13 @@
 import React from "react";
+import './productCard.css'
 
-export default function Product ({srcImg}){
+export default function Product ({srcImg, pokemonName}){
     return(
         <>
-        <div className='product'>{srcImg}</div>
+        <div className='product'>
+           <img className='pokemonImg' src={srcImg} alt={pokemonName} /> 
+           <div className='pokemonName'>{pokemonName}</div>
+        </div>
         </>
     )
 }
--- magazine/src/pages/Products.js
+++ magazine/src/pages/Products.js
@@ -1,18 +1,56 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Menu from "../components/topMenu";
 import './Products.css'
 import Product from "../components/poductCard";
 
 
-function Products({srcImg}) {
-  const colors =['aqua', 'black', 'blue', 'orange', 'pink', 'red', 'violet', 'white', 'yellow']
+function Products() {
+  const [pokemonData, setPokemonData] = useState([])
+  const [pokemonImg, setPokemonImg] = useState([])
+  useEffect(() => {
+    if(pokemonData.length === 0)
+    fetchData();
+  }, []); 
+
+  const fetchData = () => {
+    fetch(`https://pokeapi.co/api/v2/pokemon/?limit=9`)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network is not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        setPokemonData(data.results);
+  
+        const imgUrls = data.results.map(item => item.url);
+
+        Promise.all(imgUrls.map(url =>
+          fetch(url)
+            .then(response => {
+              if (!response.ok) {
+                throw new Error('Network is not ok');
+              }
+              return response.json();
+            })
+            .then(data => data.sprites.front_default)
+        ))
+        .then(imageUrls => {
+          setPokemonImg(imageUrls);
+        })
+        .catch(error => console.error('Error fetching Pokémon data:', error));
+      })
+      .catch(error => console.error('Error fetching Pokémon list:', error));
+  };
+  
+  
   return (
    <>
    <Menu/>
    <div className="productMarkup" >
-    {colors.map((element, index)=>
-    <div className="card_container">
-    <Product srcImg={element} key={index}/>
+    {pokemonData.map((element, index)=>
+    <div className="card_container" key={index}>
+    <Product srcImg={pokemonImg[index]} pokemonName={element.name}/>
     </div>
     )}
    </div>
