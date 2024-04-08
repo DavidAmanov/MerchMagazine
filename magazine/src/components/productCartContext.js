a81cc0632b7e148267d46bdba1e8f80499e87ad0
David Ladaev <174save@gmail.com>
2024-04-08 11:36:53 +0300

    delete unuse code

--- magazine/src/components/productCartContext.js
+++ magazine/src/components/productCartContext.js
@@ -2,32 +2,4 @@ import React, { createContext, useState } from "react";
 
 export const productContext = createContext([]);
 
-// export const ProductProvider = ({ children }) => {
-//     const [products, setProducts] = useState([]);
 
-//     const addProductToCart = (product) => {
-//         setProducts([...products, product]);
-//     };
-
-//     const removeProductFromCart = (productName) => {
-//         const updatedProducts = products.filter(product => product.name !== productName);
-//         setProducts(updatedProducts);
-//     };
-
-//     const cleanCart = () => {
-//         setProducts([]);
-//     };
-
-//     return (
-//         <productContext.Provider
-//             value={{
-//                 products,
-//                 addProductToCart,
-//                 removeProductFromCart,
-//                 cleanCart
-//             }}
-//         >
-//             {children}
-//         </productContext.Provider>
-//     );
-// };
